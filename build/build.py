c254d11ea19fc3e9a426b133357bd47ae5bab4c9
crschmidt <crschmidt@crschmidt.net>
2008-11-14 21:07:04 +0000

    Build changes, better template.
    git-svn-id: http://svn.openlayers.org/trunk/doc@8361 dc9f47b5-9b13-0410-9fdd-eb0c1a62fdaf

--- build/build.py
+++ build/build.py
@@ -52,15 +52,22 @@ def run_file(in_path, out_path):
     """
     publish_file(source_path=in_path, destination_path=out_path, writer_name="html", settings_overrides={'stylesheet_path': 'build/rst.css'} ) 
 
-def build_output(root_path=".."):
+def build_output(root_path="..", out_dir=None):
     index_content = index(root_path)
-    out_dir = os.path.join(root_path, "output")
-    os.mkdir(out_dir)
+    if not out_dir:
+        out_dir = os.path.join(root_path, "output")
+    try:
+        os.mkdir(out_dir)
+    except:
+        pass
     f = open(os.path.join(out_dir, "index.html"), "w") 
     f.write(index_content)
     f.close()
     for i in doc_list(root_path):
-        os.mkdir(os.path.join(out_dir, i['name']))
+        try:
+            os.mkdir(os.path.join(out_dir, i['name']))
+        except:
+            pass
         run_file(i['path'], os.path.join(out_dir, i['name'], "index.html"))
 
 def _test():
